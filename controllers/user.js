d2a99a5d6c048cfc74efc2a1859d353a12f2fb32
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-09-19 23:15:37 +0100

    add controllers

---
+++ controllers/user.js
@@ -0,0 +1,16 @@
+const User = require("../models/user")
+
+
+
+
+
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword,
+  };
+  
+  
