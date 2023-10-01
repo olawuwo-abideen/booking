95ff52f339d6da9addfc02294b59fac5beae30c9
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-10-01 20:59:44 +0100

    fix

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,13 +3,13 @@ const app = express();
 app.use(express.json());
 const bookingRouter = express.Router();
 
-const { ReserveARoom,retrievAllReservedRoom,retrievAReservedRoom } = require('../controllers/booking');
+const { ReserveARoom,retrievAllReservedRoom } = require('../controllers/booking');
 
 
 
 bookingRouter.post('/bookings',ReserveARoom);
 bookingRouter.get('/user-bookings',retrievAllReservedRoom);
-bookingRouter.get('/user-booking',retrievAReservedRoom);
+
 
 
 
